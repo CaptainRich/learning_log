4cd6487733e201c5c4b201ccce5fbeedacf7d3d3
RichA <rich_ay@sbcglobal.net>
2024-04-07 23:54:21 -0500

    Working on base/child pages, added 'topics' page.

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -5,9 +5,12 @@ from . import views              # Import 'views.py' from the same directory as 
 
 app_name = 'learning_logs'
 
-urlpatterns = {
+urlpatterns = [
     # The home page. A list of pages that can be requested from this app.  The '' empty
     # string matches the base URL.  The 2nd argument defines which function to call in
     # views.py.  The 3rd argument defines a name for this pattern for later reference. 
-    path( '', views.index, name='index' )  # 
-}
+    path( '', views.index, name='index' ),
+
+    # The URL pattern for the 'Topics' page.
+    path( 'topics', views.topics, name='topics' ),
+]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Topic
 
 # Create your views here.
 
@@ -6,4 +7,12 @@ def index( request ):
     """ The home page for the Learning Log. """
     # The 2nd argument is the template to build the web page.
     return render( request, 'learning_logs/index.html' )
+
+
+def topics( request ):
+    """ Show all the defined topics. """
+    topics = Topic.objects.order_by( 'date_added' )  # Query the database
+    context = { 'topics': topics }                   # Setup the dictionary with the data
+    return render( request, 'learning_logs/topics.html', context )
+
 
