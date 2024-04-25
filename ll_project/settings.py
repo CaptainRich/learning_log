724381a812cf15deabad24e0947dcd5774ed5a82
RichA <rich_ay@sbcglobal.net>
2024-04-25 16:50:46 -0500

    Updated error pages for 400 & 500.

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%^1%b$&^7ln1yin3=lqq+a95y6v%-m-65b$ob9#vi!60k&k4uh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
@@ -59,10 +59,13 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'll_project.urls'
 
+# The 'DIRS' line tells Django to look in the root template directory
+# for the error page templates, and any other templates that are not
+# associated with a particular App.
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
